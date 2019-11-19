3c1e1e1298e5ff6887a6a2c7883607921f80dba0
arinavictor <arinavictor@yahoo.com>
2019-11-19 10:32:37 -0700

    updates spacing in routes file and deploys to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :events
+resources :events, only: [:index, :show, :create]
 resources :categories, only: [:index, :show]
 
 end
