41728a2ccb0b5be64471a56d19be81f20ca7ad16
arinavictor <arinavictor@yahoo.com>
2019-11-21 11:14:50 -0700

    troubleshoots cors error after deployment

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://fall-o-rado.firebaseapp.com'
+    origins '*'
 
-    resource 'https://fall-o-rado.firebaseapp.com',
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
