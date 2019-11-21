20742502cc66640e07cdb153cfe455327d539878
arinavictor <arinavictor@yahoo.com>
2019-11-21 11:07:59 -0700

    changes cors origin to frontend delpoyment link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://fall-o-rado.firebaseapp.com'
 
     resource 'https://fall-o-rado.firebaseapp.com',
       headers: :any,
